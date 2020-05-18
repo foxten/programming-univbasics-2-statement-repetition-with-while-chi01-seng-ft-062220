b611b0bb156348ac00acac8145bc31a92487cb38
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-18 00:28:39 +0000

    Automatically backed up by Learn

--- spec/count_down_spec.rb
+++ spec/count_down_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe 'lib/countdown.rb' do
   it 'should output the numbers 10 through 1 in descending order' do
-    file = Dir.pwd + "/lib/count_down.rb"
+    file = Dir.pwd + "/lib/countdown.rb"
     
     expect{ load file }.to output(/10/).to_stdout
     expect{ load file }.to output(/9/).to_stdout
@@ -18,7 +18,7 @@ describe 'lib/countdown.rb' do
   end
 
   it 'should output "Happy New Year!" after the countdown' do
-    file = Dir.pwd + "/lib/count_down.rb"
+    file = Dir.pwd + "/lib/countdown.rb"
     expect{ load file }.to output(/Happy New Year!/).to_stdout
   expect{ load file }.to output(/10(\r\n|\r|\n)9(\r\n|\r|\n)8(\r\n|\r|\n)7(\r\n|\r|\n)6(\r\n|\r|\n)5(\r\n|\r|\n)4(\r\n|\r|\n)3(\r\n|\r|\n)2(\r\n|\r|\n)1(\r\n|\r|\n)Happy New Year!/).to_stdout, "expected:\n10\n9\n8\n7\n6\n5\n4\n3\n2\n1\nHappy New Year!\n"
   end
